0f27e9592451028f58af12d8c3968e74584bc80c
Shimoda <kuri65536@hotmail.com>
2020-01-30 21:48:27 +0900

    setup.py: read a long description from readme.md

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 from setuptools import setup, find_packages  # type: ignore
 
+
+def readme():
+    # type: () -> str
+    with open('README.md') as f:
+        return f.read()
+
+
 VERSION = '0.4.1'
+
+long_description = (
+    "This is a mkdocs theme, "
+    "this supports some CSS-styles to publish your documents "
+    "with PDF or printing."
+    "Please see the 'Demo' section"
+)
 
 setup(
     name="mkdocs-theme-topdf",
@@ -8,7 +22,7 @@ setup(
     url='https://github.com/kuri65536/mkdocs-theme-topdf',
     license='MPL2',
     description='a mkdocs theme for generate pdf or printing',
-    author='shimoda',
+    author='Shimoda',
     author_email='kuri65536@hotmail.com',
     packages=find_packages(),
     include_package_data=True,
@@ -17,5 +31,7 @@ setup(
             'topdf = topdf',
         ]
     },
+    long_description_content_type='text/markdown',
+    long_description=long_description + "\n\n" + readme(),
     zip_safe=False
 )
